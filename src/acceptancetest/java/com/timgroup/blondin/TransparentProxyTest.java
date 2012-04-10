df3a6feccc6a08e3719f88bbbe5e8e67186c91a5
Tom Denley <tom.denley@youdevise.com>
2012-04-10 15:23:58 +0100

    stub first acceptance test

---
+++ src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
@@ -0,0 +1,15 @@
+package com.timgroup.blondin;
+
+import org.junit.Test;
+
+public final class TransparentProxyTest {
+
+    @Test public void
+    transparently_redirects_to_target_application() {
+        // Start stub target application at x stubbing content y
+        // Start blondin instance targetting x
+        // Issue request to blondin for y
+        // Assert response from blondin is y
+    }
+
+}
