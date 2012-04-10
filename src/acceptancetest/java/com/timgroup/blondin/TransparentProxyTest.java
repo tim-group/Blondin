e2275cf800e8d49f92b20788402a8e68f1fcbbbc
Tom Denley <tom.denley@youdevise.com>
2012-04-10 16:02:25 +0100

    flesh out first acceptance test

--- src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
@@ -1,5 +1,12 @@
 package com.timgroup.blondin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.junit.Test;
 import org.webbitserver.HttpControl;
 import org.webbitserver.HttpHandler;
@@ -7,21 +14,34 @@ import org.webbitserver.HttpRequest;
 import org.webbitserver.HttpResponse;
 import org.webbitserver.WebServer;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.webbitserver.WebServers.createWebServer;
 
 public final class TransparentProxyTest {
 
     @Test public void
-    transparently_redirects_to_target_application() {
+    transparently_redirects_to_target_application() throws Exception {
         final WebServer targetServer = createWebServer(0).add("/some/target/url", StubHandler.returning("hello, world"));
+        final BlondinServer balancer = new BlondinServer("localhost:" + targetServer.getPort());
         
-        // Start stub target application at x stubbing content y
-        // Start blondin instance targetting x
-        // Issue request to blondin for y
-        // Assert response from blondin is y
+        assertThat(contentFrom("http://localhost:" + balancer.port() + "/some/target/url"), is("hello, world"));
     }
 
-    
+    private String contentFrom(final String balancerUrlString) throws MalformedURLException, IOException {
+        final URL url = new URL(balancerUrlString);
+        final URLConnection conn = url.openConnection();
+        final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        
+        final StringBuilder responseText = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            responseText.append(inputLine);
+        }
+        in.close();
+        return inputLine;
+    }
+
     public static final class StubHandler implements HttpHandler {
         private final String responseString;
 
---
+++ src/main/java/com/timgroup/blondin/BlondinServer.java
@@ -0,0 +1,17 @@
+package com.timgroup.blondin;
+
+public final class BlondinServer {
+
+    public BlondinServer(String targetUrl) {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    public int port() {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
