ec860fdc0ec5c4bbcbffbcd3cb16fa49b1b1f99f
Tom Denley <tom.denley@youdevise.com>
2012-04-10 16:07:33 +0100

    refactor to extract trivial http client for use in acceptance tests

--- src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
@@ -1,11 +1,5 @@
 package com.timgroup.blondin;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 import org.junit.Test;
 import org.webbitserver.WebServer;
@@ -21,20 +15,7 @@ public final class TransparentProxyTest {
         final WebServer targetServer = createWebServer(0).add("/some/target/url", TrivialHttpServer.serving("hello, world"));
         final BlondinServer balancer = new BlondinServer("localhost:" + targetServer.getPort());
         
-        assertThat(contentFrom("http://localhost:" + balancer.port() + "/some/target/url"), is("hello, world"));
+        assertThat(TrivialHttpClient.contentFrom("http://localhost:" + balancer.port() + "/some/target/url"), is("hello, world"));
     }
 
-    private String contentFrom(final String balancerUrlString) throws MalformedURLException, IOException {
-        final URL url = new URL(balancerUrlString);
-        final URLConnection conn = url.openConnection();
-        final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        
-        final StringBuilder responseText = new StringBuilder();
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            responseText.append(inputLine);
-        }
-        in.close();
-        return inputLine;
-    }
 }
---
+++ src/acceptancetest/java/com/timgroup/blondin/TrivialHttpClient.java
@@ -0,0 +1,24 @@
+package com.timgroup.blondin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public final class TrivialHttpClient {
+    public static String contentFrom(final String balancerUrlString) throws MalformedURLException, IOException {
+        final URL url = new URL(balancerUrlString);
+        final URLConnection conn = url.openConnection();
+        final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        
+        final StringBuilder responseText = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            responseText.append(inputLine);
+        }
+        in.close();
+        return inputLine;
+    }
+}
