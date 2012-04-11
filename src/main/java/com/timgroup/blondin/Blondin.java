0aceeb09196bcd0c1c072b2153f2766b2dcfec60
Tom Denley <tom.denley@youdevise.com>
2012-04-11 09:07:16 +0100

    add main class

---
+++ src/main/java/com/timgroup/blondin/Blondin.java
@@ -0,0 +1,9 @@
+package com.timgroup.blondin;
+
+public final class Blondin {
+
+    public static void main(String[] args) {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
