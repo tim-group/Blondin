a0b43039fd7a3a16549b652f94de6e408e096ae3
Tom Denley <tom.denley@youdevise.com>
2012-04-11 10:14:32 +0100

    java main should not throw exception -- to allow for deployment testing

--- src/main/java/com/timgroup/blondin/Blondin.java
+++ src/main/java/com/timgroup/blondin/Blondin.java
@@ -3,7 +3,7 @@ package com.timgroup.blondin;
 public final class Blondin {
 
     public static void main(String[] args) {
-        throw new java.lang.UnsupportedOperationException();
+        System.out.println("Blondin");
     }
 
 }
