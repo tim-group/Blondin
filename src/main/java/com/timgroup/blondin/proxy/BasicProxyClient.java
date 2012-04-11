73389984815d3594db47a4957c480328f304b884
Tom Denley <tom.denley@youdevise.com>
2012-04-11 13:57:09 +0100

    refactor to use guava

--- src/main/java/com/timgroup/blondin/proxy/BasicProxyClient.java
+++ src/main/java/com/timgroup/blondin/proxy/BasicProxyClient.java
@@ -1,12 +1,13 @@
 package com.timgroup.blondin.proxy;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.webbitserver.HttpRequest;
 import org.webbitserver.HttpResponse;
+
+import com.google.common.io.ByteStreams;
 
 public final class BasicProxyClient implements ProxyClient {
 
@@ -15,17 +16,10 @@ public final class BasicProxyClient implements ProxyClient {
         try {
             final URL url = new URL(request.uri());
             final URLConnection conn = url.openConnection();
-            final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            
-            final StringBuilder responseText = new StringBuilder();
-            String contentLine;
-            while ((contentLine = in.readLine()) != null) {
-                responseText.append(contentLine);
-            }
-            in.close();
-            
             response.header("Content-type", "text/plain");
-            response.write(responseText.toString());
+            final InputStream inputStream = conn.getInputStream();
+            response.content(ByteStreams.toByteArray(inputStream));
+            inputStream.close();
             response.end();
         }
         catch(Exception e) {
