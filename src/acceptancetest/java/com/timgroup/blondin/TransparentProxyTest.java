053fc5d7b6ef6d34ed999fe1bf0488cf14bf2509
Tom Denley <tom.denley@youdevise.com>
2012-04-13 12:54:53 +0100

    convert acceptance test to start Blondin realistically using java main

--- src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/TransparentProxyTest.java
@@ -1,21 +1,35 @@
 package com.timgroup.blondin;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
-import com.timgroup.blondin.server.BlondinServer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public final class TransparentProxyTest {
 
+    private final int blondinPort = 23453;
+    private final int targetPort = 34297;
+    
+    @Before
+    public void startBlondin() throws Exception {
+        Blondin.main(new String[] {String.valueOf(blondinPort), "localhost", String.valueOf(targetPort)});
+        TrivialHttpClient.waitForSocket("localhost", blondinPort);
+    }
+    
+    @After
+    public void stopBlondin() throws Exception {
+        TrivialHttpClient.post(String.format("http://localhost:%s/shutdown", blondinPort));
+        TrivialHttpClient.waitForNoSocket("localhost", blondinPort);
+    }
+    
     @Test public void
     transparently_redirects_to_target_application() throws Exception {
-        TrivialHttpServer.serving("/some/target/url", "hello, world").on(34297);
+        TrivialHttpServer.serving("/some/target/url", "hello, world").on(targetPort);
         
-        new BlondinServer(23453, "localhost", 34297);
-        
-        assertThat(TrivialHttpClient.contentFrom("http://localhost:23453/some/target/url"), is("hello, world"));
+        final String requestUrl = String.format("http://localhost:%s/some/target/url", blondinPort);
+        assertThat(TrivialHttpClient.contentFrom(requestUrl), is("hello, world"));
     }
 }
