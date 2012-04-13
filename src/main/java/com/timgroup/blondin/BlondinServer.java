ed241cd4e66a3d7788d7a26a76161931927ef6fa
Tom Denley <tom.denley@youdevise.com>
2012-04-13 12:36:49 +0100

    refactor to use dispatcher

--- src/main/java/com/timgroup/blondin/BlondinServer.java
+++ src/main/java/com/timgroup/blondin/BlondinServer.java
@@ -11,6 +11,8 @@ import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
 
 import com.timgroup.blondin.proxy.BasicHttpClient;
+import com.timgroup.blondin.proxy.HttpClient;
+import com.timgroup.blondin.server.RequestDispatcher;
 
 public final class BlondinServer {
 
@@ -18,24 +20,12 @@ public final class BlondinServer {
     private Connection connection;
 
     public BlondinServer(final int blondinPort, final String targetHost, final int targetPort) {
-        final BasicHttpClient httpClient = new BasicHttpClient();
-        final Container container = new Container() {
-            @Override
-            public void handle(Request request, Response response) {
-                try {
-                    if ("/shutdown".equals(request.getPath().getPath()) && "POST".equals(request.getMethod())) {
-                        response.close();
-                        shutdown();
-                        return;
-                    }
-                    httpClient.handle(targetHost, targetPort, request, response);
-                }
-                catch (Exception e) {
-                }
-            }
-        };
+        final RequestDispatcher dispatcher = new RequestDispatcher();
+        dispatcher.register("POST", "/shutdown", new ShutdownHandler());
+        dispatcher.register("GET", new ProxyingHandler(targetHost, targetPort, new BasicHttpClient()));
+        
         try {
-            connection = new SocketConnection(container);
+            connection = new SocketConnection(dispatcher);
             SocketAddress address = new InetSocketAddress(blondinPort);
             connection.connect(address);
         }
@@ -56,5 +46,32 @@ public final class BlondinServer {
         catch (IOException e) {
         }
         available = false;
+    }
+    
+    private final class ShutdownHandler implements Container {
+        @Override public void handle(Request request, Response response) {
+            try {
+                response.close();
+                shutdown();
+                return;
+            }
+            catch (Exception e) { }
+        }
+    }
+    
+    private static final class ProxyingHandler implements Container {
+        private final String targetHost;
+        private final int targetPort;
+        private final HttpClient httpClient;
+        
+        public ProxyingHandler(String targetHost, int targetPort, HttpClient httpClient) {
+            this.targetHost = targetHost;
+            this.targetPort = targetPort;
+            this.httpClient = httpClient;
+        }
+        
+        @Override public void handle(Request request, Response response) {
+            httpClient.handle(targetHost, targetPort, request, response);
+        }
     }
 }
