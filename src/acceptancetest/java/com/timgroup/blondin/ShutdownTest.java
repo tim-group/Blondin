f6d3c1634d41b10b7aaf779c0138946eebe24c0f
Tom Denley <tom.denley@youdevise.com>
2012-04-16 09:56:15 +0100

    use acceptance test base superclass

--- src/acceptancetest/java/com/timgroup/blondin/ShutdownTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/ShutdownTest.java
@@ -6,16 +6,13 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public final class ShutdownTest {
+public final class ShutdownTest extends BlondinAcceptanceTestBase {
 
     @Test public void
     responds_to_a_shutdown_request() throws Exception {
-        Blondin.main(new String[] {"21454", "localhost", "80"});
-        TrivialHttpClient.waitForSocket("localhost", 21454);
+        TrivialHttpClient.post(blondinUrl() + "/shutdown");
         
-        TrivialHttpClient.post("http://localhost:21454/shutdown");
-        
-        TrivialHttpClient.waitForNoSocket("localhost", 21454);
-        assertThat(TrivialHttpClient.isSocketOpen("localhost", 21454), is(false));
+        TrivialHttpClient.waitForNoSocket("localhost", blondinPort());
+        assertThat(TrivialHttpClient.isSocketOpen("localhost", blondinPort()), is(false));
     }
 }
