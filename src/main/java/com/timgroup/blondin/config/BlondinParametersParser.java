dad4ec796cf8d217b4f3f052edeaf4af95d3ed10
Tom Denley <tom.denley@youdevise.com>
2012-04-18 12:31:04 +0100

    clean up the parameters parser

--- src/main/java/com/timgroup/blondin/config/BlondinParametersParser.java
+++ src/main/java/com/timgroup/blondin/config/BlondinParametersParser.java
@@ -5,34 +5,31 @@ import java.util.Properties;
 
 import com.google.common.base.Optional;
 
+import static java.lang.Integer.parseInt;
+
 public final class BlondinParametersParser {
-    
+
     public Optional<BlondinConfiguration> parse(String[] args) {
         if (args.length == 0) {
             return Optional.absent();
         }
 
         String propertiesFilename = args[0];
-        String defaultPort = null;
+        String defaultPortString = null;
         if (args.length > 1 && isInteger(args[0])) {
-            defaultPort = args[0];
+            defaultPortString = args[0];
             propertiesFilename = args[1];
         }
 
-        final int port;
-        final String targetHost;
-        final int targetPort;
         try {
-            Properties prop = new Properties();
+            final Properties prop = new Properties();
             prop.load(new FileInputStream(propertiesFilename));
-            port = Integer.parseInt(prop.getProperty("port", defaultPort));
-            targetHost = prop.getProperty("targetHost").toString();
-            targetPort = Integer.parseInt(prop.getProperty("targetPort"));
+            return Optional.of(new BlondinConfiguration(parseInt(prop.getProperty("port", defaultPortString)),
+                                                        prop.getProperty("targetHost").toString(),
+                                                        parseInt(prop.getProperty("targetPort"))));
         } catch (Exception e) {
             return Optional.absent();
         }
-        
-        return Optional.of(new BlondinConfiguration(port, targetHost, targetPort));
     }
 
     private static boolean isInteger(String value) {
