bfd01b1596be2c00ff652624b54ddce5bcdc542e
Tom Denley <tom.denley@youdevise.com>
2012-04-20 16:07:29 +0100

    throttle expensive resources

--- src/main/java/com/timgroup/blondin/server/BlondinServer.java
+++ src/main/java/com/timgroup/blondin/server/BlondinServer.java
@@ -15,6 +15,7 @@ import com.google.common.base.Supplier;
 import com.timgroup.blondin.config.ExpensiveResourceListLoader;
 import com.timgroup.blondin.proxy.BasicHttpClient;
 import com.timgroup.blondin.proxy.ProxyingHandler;
+import com.timgroup.blondin.throttler.ThrottlingHandler;
 
 import static com.timgroup.blondin.server.RequestDispatcher.GET;
 
@@ -34,11 +35,16 @@ public final class BlondinServer {
     };
 
     public BlondinServer(int blondinPort, String targetHost, int targetPort, URL expensiveResourcesUrl) {
+        final ExpensiveResourceListLoader expensiveResourcesListSupplier = new ExpensiveResourceListLoader(expensiveResourcesUrl);
+
         final RequestDispatcher dispatcher = new RequestDispatcher();
         dispatcher.register(POST.forPath("/stop"), new StopHandler());
         dispatcher.register(POST.forPath("/suspend"), new SuspendHandler());
-        dispatcher.register(GET.forPath("/status"), new StatusPageHandler(statusSupplier, new ExpensiveResourceListLoader(expensiveResourcesUrl)));
-        dispatcher.register(GET, new ProxyingHandler(targetHost, targetPort, new BasicHttpClient()));
+        dispatcher.register(GET.forPath("/status"), new StatusPageHandler(statusSupplier, expensiveResourcesListSupplier));
+        
+        final ProxyingHandler proxy = new ProxyingHandler(targetHost, targetPort, new BasicHttpClient());
+        dispatcher.register(GET.forPath(expensiveResourcesListSupplier), new ThrottlingHandler(proxy, 16));
+        dispatcher.register(GET, proxy);
         
         try {
             connection = new SocketConnection(new ContainerServer(dispatcher, THREAD_COUNT));
