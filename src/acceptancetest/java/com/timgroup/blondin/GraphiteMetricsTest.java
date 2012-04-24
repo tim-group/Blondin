bf44648df9f362cdb5a5f0f1b74d122230b70aaf
Tom Denley <tom.denley@youdevise.com>
2012-04-24 16:44:29 +0100

    pending acceptance test for graphite connection

---
+++ src/acceptancetest/java/com/timgroup/blondin/GraphiteMetricsTest.java
@@ -0,0 +1,28 @@
+package com.timgroup.blondin;
+
+import java.net.ServerSocket;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.timgroup.blondin.testutil.BlondinAcceptanceTestBase;
+
+public final class GraphiteMetricsTest extends BlondinAcceptanceTestBase {
+
+    @Override
+    protected void beforeBlondinStartsUpWith(Properties properties, List<String> expensiveResources) throws Exception {
+        properties.setProperty("graphite.host", "localhost");
+        properties.setProperty("graphite.port", "22222");
+        properties.setProperty("graphite.period", "1");
+        properties.setProperty("graphite.periodunit", "MILLISECONDS");
+    }
+
+    @Ignore("pending implementation")
+    @Test(timeout=5000) public void
+    blondin_connects_to_graphite() throws Exception {
+        final ServerSocket graphite = new ServerSocket(22222);
+        graphite.accept();
+    }
+}
