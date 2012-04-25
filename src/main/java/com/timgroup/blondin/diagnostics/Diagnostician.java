bdb3f41a221614dfb3efe091554e1805aa98110e
Tom Denley <tom.denley@youdevise.com>
2012-04-25 10:02:51 +0100

    plot rather than monitor

--- src/main/java/com/timgroup/blondin/diagnostics/Diagnostician.java
+++ src/main/java/com/timgroup/blondin/diagnostics/Diagnostician.java
@@ -1,6 +1,5 @@
 package com.timgroup.blondin.diagnostics;
 
-import com.google.common.base.Supplier;
 
 public interface Diagnostician {
 
@@ -10,5 +9,5 @@ public interface Diagnostician {
     void logWarning(Class<?> source, String message, Throwable cause);
 
     // Metrics
-    void monitor(String aspect, Supplier<Integer> probe);
+    void plot(String aspect, Integer value);
 }
