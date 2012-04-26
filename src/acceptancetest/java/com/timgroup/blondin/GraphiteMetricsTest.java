ce0c11ffc78da0200c8d3b09c586989e1afbd870
Tom Denley <tom.denley@youdevise.com>
2012-04-26 11:51:52 +0100

    unused import

--- src/acceptancetest/java/com/timgroup/blondin/GraphiteMetricsTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/GraphiteMetricsTest.java
@@ -12,12 +12,10 @@ import com.timgroup.blondin.testutil.DummyGraphiteServer;
 import com.timgroup.blondin.testutil.TrivialHttpClient;
 import com.timgroup.blondin.testutil.TrivialHttpServer;
 
-import static org.hamcrest.Matchers.startsWith;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
 
 public final class GraphiteMetricsTest extends BlondinAcceptanceTestBase {
 
