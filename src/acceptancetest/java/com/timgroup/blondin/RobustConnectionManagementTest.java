970896e60fcee3b675fdeeb8ee8f9f3088b10bd2
Tom Denley <tom.denley@youdevise.com>
2012-05-08 14:34:16 +0100

    implement robust connection test

--- src/acceptancetest/java/com/timgroup/blondin/RobustConnectionManagementTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/RobustConnectionManagementTest.java
@@ -1,15 +1,13 @@
 package com.timgroup.blondin;
 
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.util.concurrent.Future;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.timgroup.blondin.testutil.BlondinAcceptanceTestBase;
 import com.timgroup.blondin.testutil.TrivialHttpClient;
+import com.timgroup.blondin.testutil.TrivialHttpClient.TrivialResponse;
 import com.timgroup.blondin.testutil.TrivialHttpServer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,25 +20,16 @@ public final class RobustConnectionManagementTest extends BlondinAcceptanceTestB
         TrivialHttpClient.getFrom(blondinUrl() + "/some/target/url");
     }
 
-    @Ignore("pending acceptance test until we investigate why simple web isn't forwaring the request")
-    @Test(timeout=5000) public void
-    proxies_a_request_that_immediately_closes() throws Exception {
-        TrivialHttpServer server = TrivialHttpServer.on(targetPort()).serving("/my/cheap/resource", "hello, world");
-        ClientConnection conn = ClientConnection.makeRequestFor("/my/cheap/resource", blondinPort());
-        conn.disconnect();
-        waitForRequestsToBeFowardedToServer(server, 1);
-    }
-
-    @Ignore("unfinished pending acceptance test: TODO ensure that blondin itself has finished proxying disconnected request and that " +
-            "it thinks no problems have happened")
     @Test(timeout=5000) public void
     closes_server_connection_when_a_client_connection_breaks_midway_through_response_delivery() throws Exception {
-        TrivialHttpServer server = TrivialHttpServer.on(targetPort()).serving("/my/cheap/resource", "hello, world")
+        TrivialHttpServer server = TrivialHttpServer.on(targetPort())
+                                                    .serving("/my/cheap/resource", "hello, world")
                                                     .blockingAll();
         
-        ClientConnection conn = ClientConnection.makeRequestFor("/my/cheap/resource", blondinPort());
+        Future<TrivialResponse> request = TrivialHttpClient.getFromInBackground(blondinUrl() + "/my/cheap/resource");
+        
         waitForRequestsToBeFowardedToServer(server, 1);
-        conn.disconnect();
+        request.cancel(true);
         
         server.unblock();
         waitForServerToFinishDeliveringRequests(server, 1);
@@ -58,29 +47,4 @@ public final class RobustConnectionManagementTest extends BlondinAcceptanceTestB
             Thread.yield();
         };
     }
-
-    private static final class ClientConnection {
-        
-        private final Socket client;
-        private final PrintWriter output;
-
-        public static ClientConnection makeRequestFor(String urlPath, int port) throws IOException {
-            return new ClientConnection(urlPath, port);
-        }
-        
-        private ClientConnection(String urlPath, int port) throws IOException {
-            client = new Socket("localhost", port);
-            output = new PrintWriter(client.getOutputStream());
-            
-            output.print("GET " + urlPath + " HTTP/1.0\r\n");
-            output.print("\r\n");
-            output.flush();
-        }
-        
-        public void disconnect() throws IOException {
-            output.close();
-            client.close();
-        }
-    }
-
 }
--- src/acceptancetest/java/com/timgroup/blondin/testutil/TrivialHttpClient.java
+++ src/acceptancetest/java/com/timgroup/blondin/testutil/TrivialHttpClient.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.hamcrest.Matcher;
 
@@ -24,41 +25,24 @@ public final class TrivialHttpClient {
     }
     
     public static Future<TrivialResponse> getFromInBackground(final String urlString) throws IOException {
-        final FutureTask<TrivialResponse> future = new FutureTask<TrivialResponse>(new Callable<TrivialResponse>() {
+        final CallableGetRequest request = new CallableGetRequest(urlString);
+        final FutureTask<TrivialResponse> future = new FutureTask<TrivialResponse>(request) {
             @Override
-            public TrivialResponse call() throws Exception {
-                try {
-                    return TrivialHttpClient.getFrom(urlString);
-                }
-                catch (IOException e) {
-                    throw new IllegalStateException(e);
-                }
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                request.cancel();
+                return super.cancel(mayInterruptIfRunning);
             }
-        });
+        };
         new Thread(future).start();
         return future;
     }
     
     public static TrivialResponse getFrom(final String urlString) throws IOException {
-        return getFrom(urlString, "a", "b");
+        return new CallableGetRequest(urlString).call();
     }
     
     public static TrivialResponse getFrom(String urlString, String headerName, String headerValue) throws IOException {
-        final URL url = new URL(urlString);
-        final HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        conn.setInstanceFollowRedirects(false);
-        conn.setRequestProperty(headerName, headerValue);
-        Sockets.waitForSocket(url.getHost(), url.getPort());
-        final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getResponseCode() >= 400 ? conn.getErrorStream() : conn.getInputStream()));
-        final int responseCode = conn.getResponseCode();
-        
-        final StringBuilder responseText = new StringBuilder();
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            responseText.append(inputLine);
-        }
-        in.close();
-        return new TrivialResponse(responseCode, responseText.toString());
+        return new CallableGetRequest(urlString, headerName, headerValue).call();
     }
     
     public static void post(String urlString) throws IOException {
@@ -105,4 +89,53 @@ public final class TrivialHttpClient {
         return result;
     }
 
+    private static final class CallableGetRequest implements Callable<TrivialResponse> {
+        private final String urlString;
+        private final String headerName;
+        private final String headerValue;
+
+        private final AtomicReference<HttpURLConnection> connnection = new AtomicReference<HttpURLConnection>();
+
+        public CallableGetRequest(String urlString) {
+            this(urlString, "a", "b");
+        }
+
+        public CallableGetRequest(String urlString, String headerName, String headerValue) {
+            this.urlString = urlString;
+            this.headerName = headerName;
+            this.headerValue = headerValue;
+        }
+
+        @Override
+        public TrivialResponse call() throws IOException {
+            try {
+                final URL url = new URL(urlString);
+                final HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                connnection.set(conn);
+                conn.setInstanceFollowRedirects(false);
+                conn.setRequestProperty(headerName, headerValue);
+                Sockets.waitForSocket(url.getHost(), url.getPort());
+                final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getResponseCode() >= 400 ? conn.getErrorStream() : conn.getInputStream()));
+                final int responseCode = conn.getResponseCode();
+                
+                final StringBuilder responseText = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    responseText.append(inputLine);
+                }
+                in.close();
+                return new TrivialResponse(responseCode, responseText.toString());
+            }
+            catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+        public void cancel() {
+            final HttpURLConnection conn = connnection.get();
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
 }
