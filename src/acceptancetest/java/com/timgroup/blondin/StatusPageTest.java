978da5678994fad122662c1ef2df0b3bec0bb411
Tom Denley <tom.denley@youdevise.com>
2012-05-09 09:21:11 +0100

    correct acceptance test so that it passes when not running from a jar

--- src/acceptancetest/java/com/timgroup/blondin/StatusPageTest.java
+++ src/acceptancetest/java/com/timgroup/blondin/StatusPageTest.java
@@ -3,25 +3,27 @@ package com.timgroup.blondin;
 
 import org.junit.Test;
 
+import com.timgroup.blondin.server.handler.AppInfoHandler;
 import com.timgroup.blondin.testutil.BlondinAcceptanceTestBase;
 import com.timgroup.blondin.testutil.TrivialHttpClient;
 import com.timgroup.blondin.testutil.TrivialHttpClient.TrivialResponse;
 
-import static org.hamcrest.Matchers.is;
-
-import static org.hamcrest.Matchers.containsString;
-
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 
 public final class StatusPageTest extends BlondinAcceptanceTestBase {
 
     @Test public void
     serves_a_status_page() throws Exception {
+        final String currentVersion = AppInfoHandler.class.getPackage().getImplementationVersion();
+        final String expectedVersionString = (null == currentVersion) ? "" : ": <value>" + currentVersion + "</value>";
+        
         final TrivialResponse response = TrivialHttpClient.getFrom(blondinUrl() + "/status");
         
         assertThat(response.code, is(200));
         assertThat(response.content, startsWith("<?xml version=\"1.0\" ?>"));
-        assertThat(response.content, containsString("Version: <value>"));
+        assertThat(response.content, containsString("Version" + expectedVersionString));
     }
 }
