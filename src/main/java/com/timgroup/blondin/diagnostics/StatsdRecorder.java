7c093c31be003fa5a23a77a95e150e3067ad969e
Tom Denley <tom.denley@youdevise.com>
2012-05-17 11:59:52 +0100

    non-blocking statsd recorder

--- src/main/java/com/timgroup/blondin/diagnostics/StatsdRecorder.java
+++ src/main/java/com/timgroup/blondin/diagnostics/StatsdRecorder.java
@@ -3,11 +3,15 @@ package com.timgroup.blondin.diagnostics;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public final class StatsdRecorder {
 
     private final Monitor monitor;
     private final DatagramSocket clientSocket;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public StatsdRecorder(Monitor monitor, String host, int port) {
         this.monitor = monitor;
@@ -22,7 +26,30 @@ public final class StatsdRecorder {
         this.clientSocket = socket;
     }
 
-    private void send(String message) {
+    private void send(final String message) {
+        executor.execute(new Runnable() {
+            @Override public void run() {
+                blockingSend(message);
+            }
+        });
+    }
+
+    public void stop() {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(30, TimeUnit.SECONDS);
+        }
+        catch (Exception e) {
+            this.monitor.logWarning(StatsdRecorder.class, "Failed to shutdown statsd recorder", e);
+        }
+        finally {
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+        }
+    }
+
+    private void blockingSend(String message) {
         try {
             final byte[] sendData = message.getBytes();
             final DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
