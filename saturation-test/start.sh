3f5e754b45645e53661645df0352b55d61f2875f
moonpxi <paulo.schneider@gmail.com>
2012-05-18 11:07:21 +0100

    Paulo/TomD: script exit code will be the test exit code

--- saturation-test/start.sh
+++ saturation-test/start.sh
@@ -7,6 +7,9 @@ java -jar ../build/Blondin-main*.jar blondin.properties &
 blondin_pid=$!
 
 java -jar jruby-complete*.jar test.rb
+test_exit_code=$?
 
 kill $server_pid
 kill $blondin_pid
+
+exit $test_exit_code
